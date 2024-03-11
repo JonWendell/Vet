c4956a6b4512166cb0d39f7ecedd4d46fc666817
Jon Wendell Cabrera <144750558+JonWendell@users.noreply.github.com>
2024-03-11 20:42:06 +0800

    NewForVet

---
+++ routes/client.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/client', function () {
+    return 'Anyong client';
+});
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home'); // Give a name to the route
+
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+Route::get('/product1', function () {
+    return view('product1');
+})->name('product1');
+
+Route::get('/home1', function () {
+    return view('home1');
+})->name('home1');
+
+Route::get('/about2', function () {
+    return view('about2');
+})->name('about2');
+
+Route::get('/dentalClinic', function () {
+    return view('dentalClinic');
+})->name('dentalClinic');
+
+Route::get('/dentalClinic2', function () {
+    return view('dentalClinic2');
+})->name('dentalClinic2');
+
+Route::get('/map', function () {
+    return view('map');
+})->name('map');
+
